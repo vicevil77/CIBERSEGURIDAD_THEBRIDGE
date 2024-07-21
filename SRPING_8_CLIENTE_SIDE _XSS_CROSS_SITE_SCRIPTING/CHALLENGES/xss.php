bd14aad9caac5062a8f0b5d257a019a5ae60a30a
vicevil77 <120662253+vicevil77@users.noreply.github.com>
2024-07-21 09:38:52 +0200

    act

--- SRPING_8_CLIENTE_SIDE _XSS_CROSS_SITE_SCRIPTING/CHALLENGES/xss.php
+++ SRPING_8_CLIENTE_SIDE _XSS_CROSS_SITE_SCRIPTING/CHALLENGES/xss.php
@@ -19,3 +19,22 @@ if(array_key_exists("name", $_GET) && $_GET['name'] != NULL) {
 ?>
 
 <script>while(1)alert("Este mensaje saldrá indefinidamente")</script>
+
+//codigo mejorado nivel medio
+
+<?php
+
+// Implementar CSP para mejorar la seguridad contra ataques XSS
+header("Content-Security-Policy: default-src 'self'; script-src 'none';");
+
+// Verificar si hay alguna entrada y no está vacía
+if(array_key_exists("name", $_GET) && !empty($_GET['name'])) {
+
+    // Escapar caracteres especiales de HTML como  "<" ,">","&", '"' y "'"
+    // convirtiéndolos en entidades HTML de texto plano para evitar ataques XSS
+    $name = htmlspecialchars($_GET['name'], ENT_QUOTES, 'UTF-8');
+
+    // imprime el parametro name junto con un mensaje de bienvenida
+    echo "<pre>Hello ${name}</pre>";
+}
+?>
